9140205a00555f9318d9cbbe6a0cdba033639203
Gan Kok Koon <kkgan@Gans-MacBook-Pro.local>
2024-02-17 20:16:25 +0800

    fixing sidebars

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  url: 'https://aelf-dev-workshop-2402.netlify.app', //`https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  baseUrl: '/', //`/${projectName}/`,
   trailingSlash: false,
 
   // GitHub pages deployment config.
