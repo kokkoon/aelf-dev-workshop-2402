431a9ff0971c7b15d846a05640cf3824992d08b6
Gan Kok Koon <kkgan@Gans-MacBook-Pro.local>
2024-02-17 19:59:46 +0800

    fixing sidebars

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/`,
+  baseUrl: `/${projectName}/`,
   trailingSlash: false,
 
   // GitHub pages deployment config.
